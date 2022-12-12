2cda5dc26ce0fb7f676bb8b5cd8ad6ca5f9192e8
Daniel-Landry-Github <120344366+Daniel-Landry-Github@users.noreply.github.com>
2022-12-12 16:50:52 -0600

    Add files via upload

--- Onboarding.ps1
+++ Onboarding.ps1
@@ -1,7 +1,9 @@
-﻿"Onboarding Script"
+﻿#"Onboarding Script"
+
 #Variable Declarations
 $FirstName = Read-Host "First Name"
 $LastName = Read-Host "Last Name"
+$FullName = "$FirstName.$LastName"
 $PhoneNumber = Read-Host "Phone Number"
 $PersonalEmail = Read-Host "Personal Email"
 $StartDate = Read-Host "Start Date"
@@ -16,10 +18,16 @@ $MirrorUser = Read-Host "User to Mirror"
 $UKGSSO = Read-Host "UKG SSO Y/N"
 $OpenAirSSO = Read-Host "OpenAir SSO Y/N"
 $NetSuiteSSO = Read-Host "NetSuite SSO Y/N"
+$TempPassword = "Welcome@123"
 
 
 #Step 1: Create user object
-New-ADUser
+    #Will use 'new-aduser' to create the object with name only THEN add info with 'set-aduser'.
+New-ADUser -SamAccountName "$FullName"
+
+#-GivenName "$FirstName" -Surname "$LastName" -DisplayName "$FirstName $LastName" -path '?' -AccountPassword (-AsSecureString "$TempPassword") -City "$Region" -Company "Sparkhound" -Department "$Department" -Description
+
+
 $NewUser = "DanLand.User"
 #Step ?: mirror security groups from target user
 $MirrorUser = Read-Host "Mirror user: ". #Request the user that will be mirrored
