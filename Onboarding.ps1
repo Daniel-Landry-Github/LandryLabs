9e91c3c44aae4e601c2f673306442196117b8c3b
Daniel-Landry-Github <120344366+Daniel-Landry-Github@users.noreply.github.com>
2022-12-12 13:53:44 -0600

    Add files via upload

--- Onboarding.ps1
+++ Onboarding.ps1
@@ -1,8 +1,15 @@
 ﻿"Onboarding Script"
 
-$NewUser = DanLand.User
+$NewUser = "DanLand.User"
 #Step ?: mirror security groups from target user
 $MirrorUser = Read-Host "Mirror user: ". #Request the user that will be mirrored
 $MirrorGroups = (get-aduser -Identity $MirrorUser -properties *).Memberof #Fetches that user's on-prem groups
-$MirrorGroups
-foreach ($MirrorGroupEntry in $MirrorGroups) {"Adding $NewUser to $MirrorGroupEntry"}
+foreach ($MirrorGroupEntry in $MirrorGroups) 
+{
+    "Adding $NewUser to $MirrorGroupEntry"
+    <#set-aduser -identity $NewUser -Add @{Memberof='$MirrorGroupEntry'}
+    (get-aduser -identity $NewUser -properties *).Memberof
+    #>
+    Add-ADGroupMember $MirrorGroupEntry $NewUser
+    $MirrorGroupVerify = (get-aduser -identity $NewUser -properties *).Memberof
+}
