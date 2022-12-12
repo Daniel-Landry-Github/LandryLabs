baff5c01560803fdd707ec6c35c0da902554ca86
Daniel-Landry-Github <120344366+Daniel-Landry-Github@users.noreply.github.com>
2022-12-12 16:08:32 -0600

    Add files via upload

--- Onboarding.ps1
+++ Onboarding.ps1
@@ -1,15 +1,42 @@
 ﻿"Onboarding Script"
+#Variable Declarations
+$FirstName = Read-Host "First Name"
+$LastName = Read-Host "Last Name"
+$PhoneNumber = Read-Host "Phone Number"
+$PersonalEmail = Read-Host "Personal Email"
+$StartDate = Read-Host "Start Date"
+$Region = Read-Host "Region"
+$Practice = Read-Host "Practice"
+$Department = Read-Host "Department"
+$Manager = Read-Host "Manager"
+$Title = Read-Host "Title"
+$EmploymentStatus = Read-Host "Full time/Part time/Contractor"
+$BusinessUnit = Read-Host "Business Unit"
+$MirrorUser = Read-Host "User to Mirror"
+$UKGSSO = Read-Host "UKG SSO Y/N"
+$OpenAirSSO = Read-Host "OpenAir SSO Y/N"
+$NetSuiteSSO = Read-Host "NetSuite SSO Y/N"
 
+
+#Step 1: Create user object
+New-ADUser
 $NewUser = "DanLand.User"
 #Step ?: mirror security groups from target user
 $MirrorUser = Read-Host "Mirror user: ". #Request the user that will be mirrored
 $MirrorGroups = (get-aduser -Identity $MirrorUser -properties *).Memberof #Fetches that user's on-prem groups
-foreach ($MirrorGroupEntry in $MirrorGroups) 
+$MirrorFunction = foreach ($MirrorGroupEntry in $MirrorGroups) 
 {
     "Adding $NewUser to $MirrorGroupEntry"
-    <#set-aduser -identity $NewUser -Add @{Memberof='$MirrorGroupEntry'}
-    (get-aduser -identity $NewUser -properties *).Memberof
-    #>
     Add-ADGroupMember $MirrorGroupEntry $NewUser
-    $MirrorGroupVerify = (get-aduser -identity $NewUser -properties *).Memberof
+}
+$PostMirrorNewUserGroups = (get-aduser -Identity $NewUser -properties *).Memberof
+$MirrorFunction
+if ($PostMirrorNewUserGroups = $MirrorUser)
+{
+    "------------------------------"
+    "All groups have been successfully mirrored from $MirrorUser."
+}
+else
+{
+    "The mirroring was not successful."
 }
