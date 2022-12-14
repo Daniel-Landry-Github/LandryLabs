c27ee60d0bc00d30f29acac3d73d5a3aefe2b6db
Daniel-Landry-Github <120344366+Daniel-Landry-Github@users.noreply.github.com>
2022-12-14 00:17:19 -0600

    Add files via upload

--- Onboarding.ps1
+++ Onboarding.ps1
@@ -3,34 +3,42 @@
 #Variable Declarations
 $FirstName = Read-Host "First Name"
 $LastName = Read-Host "Last Name"
-$FullName = "$FirstName.$LastName"
-$PhoneNumber = Read-Host "Phone Number"
-$PersonalEmail = Read-Host "Personal Email"
-$StartDate = Read-Host "Start Date"
-$Region = Read-Host "Region"
-$Practice = Read-Host "Practice"
-$Department = Read-Host "Department"
-$Manager = Read-Host "Manager"
+$FullName = "$FirstName $LastName"
 $Title = Read-Host "Title"
+$Region = Read-Host "Region"
+$PhoneNumber = Read-Host "Phone Number"
+$EmailAddress = "$FullName@sparkhound.com"
+$PersonalEmail = Read-Host "Personal Email"
+$Department = Read-Host "Department"
+$Company = "Sparkhound"
+$Manager = Read-Host "Manager"
+$MirrorUser = Read-Host "User to Mirror"
+
+$StartDate = Read-Host "Start Date"
+$Practice = Read-Host "Practice"
 $EmploymentStatus = Read-Host "Full time/Part time/Contractor"
 $BusinessUnit = Read-Host "Business Unit"
-$MirrorUser = Read-Host "User to Mirror"
+
 $UKGSSO = Read-Host "UKG SSO Y/N"
 $OpenAirSSO = Read-Host "OpenAir SSO Y/N"
 $NetSuiteSSO = Read-Host "NetSuite SSO Y/N"
-$TempPassword = "Welcome@123"
+$Password = Read-Host -AsSecureString
+
+#Establishing MSOline Connection for cloud items.
+#Connect-MsolService
+
 
 
 #Step 1: Create user object
     #Will use 'new-aduser' to create the object with name only THEN add info with 'set-aduser'.
-New-ADUser -SamAccountName "$FullName"
-Set-ADUser -Identity "$FullName" -GivenName "$FirstName" -Surname "$LastName" -DisplayName "$FirstName $LastName" -path '?' -AccountPassword (-AsSecureString "$TempPassword") -City "$Region" 
-Set-ADUser -Company "Sparkhound" -Department "$Department" -Description $Title -EmailAddress "$FullName@sparkhound.com" -Enabled:$true -Manager "$Manager" -MobilePhone "$PhoneNumber" -
+New-ADUser -Name "$FullName" -AccountPassword $Password -Enabled:$true -GivenName $FirstName -Surname $LastName -DisplayName $FullName -City $Region -Company $Company -Department $Department -Description $Title -EmailAddress $EmailAddress -Manager $Manager -MobilePhone $PhoneNumber -Title $Title
+#Set-ADUser -identity "$Firstname.$LastName" -GivenName "$FirstName" -Surname "$LastName" -DisplayName "$FirstName $LastName" -City "$Region" 
+#Set-ADUser -Company "Sparkhound" -Department "$Department" -Description $Title -EmailAddress "$EmailAddress" -Enabled:$true -Manager "$Manager" -MobilePhone "$PhoneNumber"
 
 
-$NewUser = "DanLand.User"
-#Step ?: mirror security groups from target user
-$MirrorUser = Read-Host "Mirror user: ". #Request the user that will be mirrored
+#Step 2: Mirror security groups from target user
+#$MirrorUser = Read-Host "Mirror User" #Used for isolated group adding requests.
+#$NewUser = Read-Host "New User" #Used for isolated group adding requests.
 $MirrorGroups = (get-aduser -Identity $MirrorUser -properties *).Memberof #Fetches that user's on-prem groups
 $MirrorFunction = foreach ($MirrorGroupEntry in $MirrorGroups) 
 {
@@ -48,3 +56,4 @@ else
 {
     "The mirroring was not successful."
 }
+#Step 3: Add user to applicable cloud groups (O365 license, Ultipro, netsuite, openair)
