6c4545bd1871d313b975c1e036e008c82be1f0f0
Daniel Landry <120344366+Daniel-Landry-Github@users.noreply.github.com>
2023-01-06 15:48:56 -0600

    Add files via upload
    01/05/23
    + Building logging instructions section by section.
    + Added 'Do-Until' loop to ensure a valid forwarding email is submitted. Also rejects the termed user's email.
    
    01/06/23
    o Working on logging for 'Function Program' section.
    + Logging for 'Function Program' complete.

--- Termination.ps1
+++ Termination.ps1
@@ -1,12 +1,12 @@
 <#
-#      || TERMINATION SCRIPT
+
+#      || LANDRY LABS - TERMINATION SCRIPT
 #      || SUMMARY: 1. Disables user object, logs properties/groups and wipes them, 
 # #    || -------- 2. Moves user object to designated OU for disabled user objects. 
 ####   || -------- 3. Blocks O365 sign-in, converts mailbox to shared, revokes licenses. 
   #    || 
-  #### || 
-LANDRY ||
- LABS  || Written by Daniel Landry (daniel.landry@sparkhound.com)
+  #### || Written by Daniel Landry (daniel.landry@sparkhound.com)
+
 #>
 
 <#----------TO DO:
@@ -20,6 +20,18 @@ LANDRY ||
 
 -IMPROVE:
 --Add variable for Connectwise Ticket # for reference.
+--Add/Adjust logging for the following sections:
+---'REQUEST AND VERIFY USER'
+---'REQUEST AND VERIFY MAILBOX FORWARDING TARGET'
+---'STEP 1: DISABLE THE USER OBJECT'
+---'STEP 2: LOG AND WIPE ON-PREM AD GROUPS'
+---'STEP 3: WIPE MANAGER'
+---'STEP 4: WIPE DATE OF BIRTH & DATE OF HIRE EXTENDED ATTRIBUTES'
+---'STEP 5: MOVE USER OBJECT TO DISABLED USER OU'
+---'STEP 6: BLOCK O365 SIGN-IN ACCESS'
+---'STEP 7: CONVERT MAILBOX TO SHARED'
+---'STEP 8: LOG AND REVOKE ALL LICENSES'
+---'STEP 9: ENABLE EMAIL FORWARDING (IF APPLICABLE)'
 ----------#>
 
 <#----------Change Log:
@@ -31,14 +43,23 @@ o Adjusted the labeling of each step to allow better visibility.
 + Created a function called 'Program' containing a greeting summary of the script, asking the user to confirm that they want to continue or exit.
 + Created a function called 'RequestTermInfo' that groups the username request and verification against AD in one function before proceeding.
 - Removed 'RequestTermInfo' as a function for now and back in line on the main script due to the variables and their values NOT carrying over to the main script to be further referenced.
-+ Added 'Do-While' loop to ensure a valid username is submitted.
++ Added 'Do-Until' loop to ensure a valid username is submitted.
+
+01/05/23
++ Building logging instructions section by section.
++ Added 'Do-Until' loop to ensure a valid forwarding email is submitted. Also rejects the termed user's email.
+
+01/06/23
+o Working on logging for 'Function Program' section.
++ Logging for 'Function Program' complete.
 ----------#>
 
 
 
 
-$date = Get-Date -UFormat "%D %r"
+$Date = Get-Date
 $DisabledOU = 'OU=Disabled Accounts,DC=sparkhound,DC=com'
+$LogPath = "C:\users\daniel.landry\OneDrive - Sparkhound Inc\LandryLabs\Logs\Terminations.txt"
 
 #==========^==========#
 #START OF FUNCTIONS
@@ -62,7 +83,9 @@ Function Program #SCRIPT GREETING AND REQUESTING CONFIRMATION TO START TERM OR E
                 {
                     if ($Begin -eq "Y")
                         {
+                            "Script started at $Date`n" | Out-File -FilePath $LogPath -append
                             return;
+                            
                         }
                     elseif ($Begin -eq "N")
                         {
@@ -77,47 +100,6 @@ Function Program #SCRIPT GREETING AND REQUESTING CONFIRMATION TO START TERM OR E
             until ($Begin -eq "Y" -or $Begin -eq "N") 
      }
 
-    
-<#function RequestTermInfo #REQUESTING AND VERIFYING USER INFORMATION BEFORE TERM.
-    {
-        $username = Read-Host "Enter username (First.Last)"
-        
-        $verifyUserExists = (get-aduser -filter {samaccountname -like $username}).samaccountname
-        if ($verifyUserExists -eq $username)
-            {
-                "Account confirmed! Fetching information..."
-                $Name = (get-aduser -identity $username -properties cn).cn
-                $UserPath = (get-aduser -identity $username).distinguishedname
-                $UserPathConvert = "$userPath"
-                $UserPathConvert
-                $EmailAddress = "$username@sparkhound.com"
-                "Proceeding to disable user..."
-                return $username, $name, $userPath, $UserPathConvert, $EmailAddress;
-                #DisableUser; #Moving to disable the user.
-            }
-        else
-            {
-                "Username not found. Please enter a valid username."
-                RequestTermInfo;
-            }
-            
-    }#>
-    
-Function DisableUser
-    {
-        $userEnabledStatus = (Get-aduser -Identity $username).Enabled
-            If ($userEnabledStatus = "true") 
-                {
-                    "$username is currently enabled. Disabling..."
-                    set-aduser -identity $username -Enabled $false
-                    "$username is now disabled."
-                }
-            else 
-                {
-                    "$username is already disabled...Proceeding."
-                }
-    }        
-
 #==========^==========#
 #END OF FUNCTIONS
 #==========V==========#
@@ -128,34 +110,79 @@ Program; #Script starts interaction with this 'Program' function.
 
 
 #==========^==========#
-#REQUEST AND VERIFY USER INFORMATION
+#REQUEST AND VERIFY USER
 #==========V==========#
     do 
         {
+            "--START 'REQUEST & VERIFY' LOG--`n" | Out-File -FilePath $LogPath -Append
             $username = Read-Host "Enter username (First.Last)"
-            $verifyUserExists = (get-aduser -filter {samaccountname -like $username}).samaccountname
-            if ($verifyUserExists -eq $username)
+            "LOG: SUBMITTED USERNAME: $username" | Out-File -FilePath $LogPath -Append          
+            $userVerificaiton = (get-aduser -filter {samaccountname -like $username}).samaccountname
+            "LOG: VERIFICATION RESULTS: $userVerification" | Tee-Object -FilePath $LogPath -Append
+            if ($userVerificaiton -eq $username)
                 {
-                    "Account confirmed! Fetching information..."
+                    "Account confirmed! Fetching information..." | Tee-Object -FilePath $LogPath -Append
                     $Name = (get-aduser -identity $username -properties cn).cn
                     $UserPath = (get-aduser -identity $username).distinguishedname
-                    $UserPathConvert = "$userPath"
-                    $UserPathConvert
-                    $EmailAddress = "$username@sparkhound.com"
-                    "Proceeding to disable user...";
-                    $Verified = "Y"
+                    $UserPathConvert = "$userPath" 
+                    $UserPathConvert 
+                    $EmailAddress = "$username@sparkhound.com" 
+                    $UserVerified = "Y"
+                    get-aduser -identity $username -properties * | Out-File -FilePath $LogPath -Append #Logging FULL properties list for reference.
                 }
             else
                 {
-                    $Verified = "N"
-                    "Username not found. Please enter a valid username."
+                    $UserVerified = "N" 
+                    "$username not found. Please enter a valid username." | Out-File -FilePath $LogPath -Append
                 }
         }
-    Until ($Verified -eq "Y")
+    Until ($UserVerified -eq "Y")
+
+#==========^==========#
+#REQUEST AND VERIFY MAILBOX FORWARDING TARGET
+#==========V==========#
+    Do
+        {
+            $MailboxForwarding = Read-Host "Forwarding $username's email to another mailbox? (Enter 'Y'/'N')"
+            "LOG: Forwarding set to $MailboxForwarding"
+                If ($MailboxForwarding -eq 'Y')
+                    {
+                        $MailboxForwardingAddress = Read-Host "Please enter the email address to forward to"
+                        $verifyForwardingTarget = (get-aduser -filter {userprincipalname -like $MailboxForwardingAddress}).userprincipalname
+                            
+                            if ($verifyForwardingTarget -eq $EmailAddress)
+                                {
+                                    $ForwardingVerified = "N"
+                                    "Forwarding address can not be the same as the termed user's address. Try again."
+                                }
+                            elseif ($verifyForwardingTarget -eq $MailboxForwardingAddress)
+                                {
+                                    $ForwardingVerified = "Y"
+                                    "Forwarding address confirmed! Proceeding."
+                                }
+                            else
+                                {
+                                    $ForwardingVerified = "N"
+                                    "Submitted address doesn't match to an active address."
+                                }
+                    }
+                elseif ($MailboxForwarding -eq 'N')
+                    {
+                        $ForwardingVerified = "Y"
+                        "No email forwarding will take place."
+                    }
+                else
+                    {
+                        $ForwardingVerified = "N"
+                        "Submission invalid. Please enter either 'Y' or 'N' to continue."
+                    }
+        }
+    Until ($ForwardingVerified -eq "Y")
 
 #==========^==========#
 #STEP 1: DISABLE THE USER OBJECT
 #==========V==========#
+"Proceeding to disable user...";
 $userEnabledStatus = (Get-aduser -Identity $username).Enabled
     If ($userEnabledStatus = "true") 
         {
@@ -172,12 +199,12 @@ $userEnabledStatus = (Get-aduser -Identity $username).Enabled
 #STEP 2: LOG AND WIPE ON-PREM AD GROUPS (Add cloud groups?)
 #==========V==========#
 $userGroups = (get-aduser -Identity $username -properties *).Memberof
-"Fetching groups list..."
+Write-Host "Fetching groups list..." -NoNewline; 
 "----------"
     foreach ($UserGroupEntry in $userGroups)
         {
             "`nRemoving $username from $UserGroupEntry..."
-            #Remove-ADGroupMember -identity $UserGroupEntry -member $username -Confirm:$false
+            Remove-ADGroupMember -identity $UserGroupEntry -member $username -Confirm:$false
             $userGroupRemovedVerify = (Get-ADGroupMember -Identity $UserGroupEntry).samaccountname
             foreach ($PostRemovedGroupMembers in $userGroupRemovedVerify)
             {
@@ -253,6 +280,8 @@ $UserDisabledPath = ("CN=$Name,"+$DisabledOU)
             "Object move failed"
         };
 
+Connect-AzureAD
+Connect-ExchangeOnline
 #==========^==========#
 #STEP 6: BLOCK O365 SIGN-IN ACCESS
 #==========V==========#
@@ -303,5 +332,51 @@ $MailboxGroupList = get-azureadgroupmember -objectid $MailboxGroup | ft userprin
                 }
         }
 
+#==========^==========#
+#STEP 9: ENABLE EMAIL FORWARDING (IF APPLICABLE)
+#==========V==========#
+
+set-mailbox $EmailAddress -ForwardingAddress $MailboxForwardingAddress
 
 
+
+<#
+function RequestTermInfo #REQUESTING AND VERIFYING USER INFORMATION BEFORE TERM.
+    {
+        $username = Read-Host "Enter username (First.Last)"
+        
+        $verifyUserExists = (get-aduser -filter {samaccountname -like $username}).samaccountname
+        if ($verifyUserExists -eq $username)
+            {
+                "Account confirmed! Fetching information..."
+                $Name = (get-aduser -identity $username -properties cn).cn
+                $UserPath = (get-aduser -identity $username).distinguishedname
+                $UserPathConvert = "$userPath"
+                $UserPathConvert
+                $EmailAddress = "$username@sparkhound.com"
+                "Proceeding to disable user..."
+                return $username, $name, $userPath, $UserPathConvert, $EmailAddress;
+                #DisableUser; #Moving to disable the user.
+            }
+        else
+            {
+                "Username not found. Please enter a valid username."
+                RequestTermInfo;
+            }
+            
+    }
+    
+Function DisableUser
+    {
+        $userEnabledStatus = (Get-aduser -Identity $username).Enabled
+            If ($userEnabledStatus = "true") 
+                {
+                    "$username is currently enabled. Disabling..."
+                    set-aduser -identity $username -Enabled $false
+                    "$username is now disabled."
+                }
+            else 
+                {
+                    "$username is already disabled...Proceeding."
+                }
+    }#>
